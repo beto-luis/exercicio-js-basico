c8bf6474dc9c1b4a3ae92bd1eabe8abb6aa027a6
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-15 18:12:29 -0300

    alert

--- exercicio2.js
+++ exercicio2.js
@@ -1,7 +1,7 @@
 const boletoPago = false
 
 if(boletoPago){
-    console.log("O boleto está pago")
+    alert("O boleto está pago")
 }else{
-    console.log("O boleto não está pago")
+    alert("O boleto não está pago")
 }
