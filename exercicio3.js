50f6d6850e18b7be60c61006294cd5a9879851e0
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-15 18:39:46 -0300

    Exercício 3 concluído

---
+++ exercicio3.js
@@ -0,0 +1,3 @@
+const respostaDoAventureiro = confirm('Seja bem vindo(a) aventureiro(a)! Gostaria de pagar 50,00 para passar a noite na nossa hospedagem?')
+
+respostaDoAventureiro === true ? alert('Ótimo! Nós temos as melhores camas de toda a região!') : alert('Que pena! Você parecia ser uma pessoa mais legal')
