80f509a97d41c19b7e96bbf5a96e9aab4922a34f
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-15 21:39:06 -0300

    Exercício 4 concluído

--- exercicio3.js
+++ exercicio3.js
@@ -1,3 +1,11 @@
 const respostaDoAventureiro = confirm('Seja bem vindo(a) aventureiro(a)! Gostaria de pagar 50,00 para passar a noite na nossa hospedagem?')
 
 respostaDoAventureiro === true ? alert('Ótimo! Nós temos as melhores camas de toda a região!') : alert('Que pena! Você parecia ser uma pessoa mais legal')
+
+//Correção do exercício, porém utilizar operador ternário também funciona.
+
+// if(respostaDoAventureiro){
+//     alert('Ótimo! Nós temos as melhores camas de toda a região!')
+// }else{
+//     alert('Que pena! Você parecia ser uma pessoa mais legal')
+// }
